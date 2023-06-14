c395d8f706e8b06a2f9c41e3aa83ce3709d8c6c5
prashanthreddy <a.prashanth5reddy@gmail.com>
2023-06-14 14:45:18 +0000

    updated

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,5 +1,5 @@
 import CartContext from "../../context/CartContext";
-
+import Cookies from "js-cookie";
 import Header from "../Header";
 import EmptyCartView from "../EmptyCartView";
 import CartListView from "../CartListView";
@@ -7,40 +7,55 @@ import CartSummary from "../CartSummary";
 
 import "./index.css";
 
-const Cart = () => (
-  <CartContext.Consumer>
-    {(value) => {
-      const { cartList, removeAllCartItems } = value;
-      const showEmptyView = cartList.length === 0;
-      const onClickRemoveAllBtn = () => {
-        removeAllCartItems();
-      };
+const getCartList = async () => {
+    const jwtToken = Cookies.get("jwt_token");
+    const apiUrl = `http://localhost:3004/cart`;
+    const options = {
+        headers: {
+            Authorization: `Bearer ${jwtToken}`,
+        },
+        method: "GET",
+    };
+    const response = await fetch(apiUrl, options);
+    const cartList = await response.json()
+    console.log(cartList)
+};
+getCartList()
 
-      return (
-        <>
-          <Header />
-          <div className="cart-container">
-            {showEmptyView ? (
-              <EmptyCartView />
-            ) : (
-              <div className="cart-content-container">
-                <h1 className="cart-heading">My Cart</h1>
-                <button
-                  type="button"
-                  className="remove-all-btn"
-                  onClick={onClickRemoveAllBtn}
-                >
-                  Remove All
+const Cart = () => (
+    <CartContext.Consumer>
+        {(value) => {
+            const { cartList, removeAllCartItems } = value;
+            const showEmptyView = cartList.length === 0;
+            const onClickRemoveAllBtn = () => {
+                removeAllCartItems();
+            };
+
+            return (
+                <>
+                    <Header />
+                    <div className="cart-container">
+                        {showEmptyView ? (
+                            <EmptyCartView />
+                        ) : (
+                                <div className="cart-content-container">
+                                    <h1 className="cart-heading">My Cart</h1>
+                                    <button
+                                        type="button"
+                                        className="remove-all-btn"
+                                        onClick={onClickRemoveAllBtn}
+                                    >
+                                        Remove All
                 </button>
-                <CartListView />
-                <CartSummary />
-              </div>
-            )}
-          </div>
-        </>
-      );
-    }}
-  </CartContext.Consumer>
+                                    <CartListView />
+                                    <CartSummary />
+                                </div>
+                            )}
+                    </div>
+                </>
+            );
+        }}
+    </CartContext.Consumer>
 );
 
 export default Cart;
